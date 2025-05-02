77bc6b5a698f8b740d73724f55fdb35924b6756b
Yusuf Baskeser <yusufbaskeser2005@gmail.com>
2025-05-02 12:11:33 +0300

    Homeworks2

--- homeworks2/less8.js
+++ homeworks2/less8.js
@@ -1,27 +1,24 @@
 function lesswords(dizi) {
-    let sayac = {};
-    let sonuc = [];
+  let sayac = {};
+  let sonuc = '';
   
-    for (let i = 0; i < dizi.length; i++) {
-      let eleman = dizi[i];
-      if (sayac[eleman] === undefined) {
-        sayac[eleman] = 1;
-      } else {
-        sayac[eleman]++;
-      }
+  for (let i = 0; i < dizi.length; i++) {
+    let eleman = dizi[i];
+    if (sayac[eleman] === undefined) {
+      sayac[eleman] = 1;
+    } else {
+      sayac[eleman]++;
     }
-  
-    for (let i = 0; i < dizi.length; i++) {
-      let eleman = dizi[i];
-      if (sayac[eleman] === 1) {
-        sonuc.push(eleman);
-      }
-    }
-  
-    return sonuc;
   }
-  
-  
-  
-  console.log(lesswords(['a', 'b', 'c', 'a', 'd', 'e', 'b']));
-  
+
+  for (let i = 0; i < dizi.length; i++) {
+    let eleman = dizi[i];
+    if (sayac[eleman] === 1) {
+      sonuc += eleman;
+    }
+  }
+
+  return sonuc;
+}
+
+console.log(lesswords(['a', 'b', 'c', 'a', 'd', 'e', 'b']));
